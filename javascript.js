740e82846a870bd1548a7abdc9605ce9d4d70942
Wesliet <44872914+Wesliet@users.noreply.github.com>
2018-11-16 03:13:51 +0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+setTimeout(function() {
+	if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Pour accéder à ce site vous devez avoir 18 ans ou plus, cliquez sur "OK" si c\'est le cas.'))
+{
+    alert('Vous allez être redirigé vers le site.');
+	window.location =" https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+else 
+{
+    alert("Désolé, vous n'avez pas accès à ce site.");
+}}, 10000);
